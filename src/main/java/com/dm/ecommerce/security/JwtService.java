6b23ffdbf3082cd6b69850d1f26cbe0261143deb
NCristofe <cristofegit@gmail.com>
2026-04-29 08:01:51 -0300

    Implement JWT token service

---
+++ src/main/java/com/dm/ecommerce/security/JwtService.java
@@ -0,0 +1,42 @@
+package com.dm.ecommerce.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final String SECRET = "minha-chave-super-secreta-minha-chave-super-secreta";
+
+    private SecretKey getSignKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean isValid(String token, String username) {
+        return extractUsername(token).equals(username);
+    }
+}
